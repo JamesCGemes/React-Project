9b05eec6a0be4cfa75711ae756962d7e03448738
James Gemes <james.gemes@gmail.com>
2021-04-16 12:15:02 -0500

    working on components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {database} from "firebase";
+
+class App extends React.Component{
+    render() {
+        return(
+            <div className="catch-of-the-day">
+                <p>Howdy</p>
+            </div>
+        )
+    }
+}
+
+export default App
