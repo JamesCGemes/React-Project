0feb75a1452d8cd3bef8d38c3b9c9c15692e7b9d
James Gemes <james.gemes@gmail.com>
2021-04-16 14:51:56 -0500

    Finished skeleton for header, inventory and orders components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import {database} from "firebase";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
 
 class App extends React.Component{
     render() {
         return(
             <div className="catch-of-the-day">
-                <p>Howdy</p>
+                <div className="menu">
+                    <Header />
+                </div>
+                <Order />
+                <Inventory />
+                <header/>
             </div>
         )
     }
