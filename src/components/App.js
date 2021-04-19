eae79f94c7e3ecf2a795abc2a18e2daa50b8993a
James Gemes <james.gemes@gmail.com>
2021-04-19 10:23:55 -0500

    added helper functions

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,6 @@ class App extends React.Component{
                 </div>
                 <Order />
                 <Inventory />
-                <header/>
             </div>
         )
     }
