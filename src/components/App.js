bf19449dcb5f44fa59356f3016d80a7b6bd94427
James Gemes <james.gemes@gmail.com>
2021-04-21 15:29:24 -0500

    owrking on store name showing in firebase RTDB

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
 import React from "react";
-import {database} from "firebase";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "./sample-fish";
 import Fish from "./Fish";
+import base from "../base";
+import fishes from "./sample-fish";
 
 class App extends React.Component{
     state = {
         fishes: {},
         order: {}
     };
+
+    componentDidMount() {
+        const {params} = this.props.match;
+        this.ref = base.syncState(`${this.params.storeId}/fishes`, {
+            context: this,
+            state: "fishes"
+        });
+    }
+
+
     addFish = fish => {
         //This makes a copy of whats in state.
         const fishes = {...this.state.fishes};
@@ -25,6 +36,16 @@ class App extends React.Component{
     loadSampleFishes = () => {
         this.setState({fishes: sampleFishes});
     };
+
+    addToOrder = (key) => {
+        // Take a copy of the state
+        const order = {...this.state.order};
+        // Either add to the order or update the number in the order
+        order[key] = order[key] + 1 || 1;
+        // Call setstate to update our state object
+        this.setState({order})
+    }
+
     render() {
         return(
             <div className="catch-of-the-day">
@@ -36,11 +57,12 @@ class App extends React.Component{
                                 key={key}
                                 index={key}
                                 details={this.state.fishes[key]}
+                                addToOrder={this.addToOrder}
                             />
                         ))}
                     </ul>
                 </div>
-                <Order />
+                <Order fishes={this.state.fishes} order={this.state.order} />
                 <Inventory
                     addFish={this.addFish}
                     loadSampleFishes={this.loadSampleFishes}
